80909b78ed04f53deeb3cfd2412c471ab8120c1b
Andrew Walker <andrew.walker@resolver.com>
2016-06-14 14:36:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+hexo.extend.tag.register('tag_url', function(args, content){
+  return `/tags/${args[0]}`;
+});
+
+hexo.extend.tag.register('tag_link', function (args, content) {
+  var tag = args[0] || '';
+  var label = args[1] || tag;
+  var url = `/tags/${tag.replace(' ', '-')}/`;
+
+  return `<a href='${url}'>${label}</a>`;
+});
